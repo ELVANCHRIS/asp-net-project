2ec81aef1d9c58796ce6774e079d5b7eef382eea
ELVANCHRIS <150577337+ELVANCHRIS@users.noreply.github.com>
2025-02-01 09:53:00 +0300

    Add files via upload

---
+++ calculatorApp2/calculatorApp2/CalculatorApp.aspx
@@ -0,0 +1,77 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="CalculatorApp.aspx.cs" Inherits="calculatorApp2.CalculatorApp" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+	<title>Calculator</title>
+
+<style>
+*{
+	padding: 0;
+	margin: 0;
+	box-sizing: border-box;
+}
+
+body{
+	width: 100vw;
+	height: 100vh;
+	position:relative;
+}
+
+.calic {
+	position:relative;
+	width:500px;
+	height: 350px;
+	background-color: white;
+	top: 50%;
+	left: 50%;
+	transform: translateX(-50%) translateY(-50%);
+	display:flex;
+	align-content:center;
+	justify-content:space-evenly;
+	flex-direction:column;
+	padding:20px;
+	border: 2px solid black;
+	box-shadow: 5px 5px 10px rgba(0, 0, 0, 0.3);
+}
+div{
+	width:100%;
+	display: flex;
+	justify-content:space-between;
+	align-items:center;
+}
+
+.text_box{
+	width: 60%;
+	height: 30px;
+}
+.btn{
+	width: 100px;
+	height: 40px;
+}
+</style>
+</head>
+<body>
+	<form id="form1" runat="server" class="calic">
+		<div>
+			<label for="firstNum">Your First Number</label>
+			<asp:TextBox ID="firstNum" runat="server" CssClass="text_box"></asp:TextBox>
+		</div>
+		<div>
+			<label for="secondNum">Your Second Number</label>
+			<asp:TextBox ID="secondNum" runat="server" CssClass="text_box"></asp:TextBox>
+		</div>
+		<div>
+			<asp:Button ID="additionBtn" runat="server" CssClass="btn" Text="Addition" onclick="Button_Click"/>
+			<asp:Button ID="divisionBtn" runat="server" CssClass="btn" Text="Division" onclick="Button_Click"/>
+			<asp:Button ID="subtractionBtn" runat="server" CssClass="btn" Text="Subtraction" onclick="Button_Click"/>
+			<asp:Button ID="multiplicationBtn" runat="server" CssClass="btn" Text="Multiplication" onclick="Button_Click"/>
+		</div>
+		<div>
+			<label for="output">Output</label>
+			<asp:TextBox ID="Output" runat="server" CssClass="text_box" ReadOnly="true"></asp:TextBox>
+		</div>
+	</form>
+</body>
+</html>
---
+++ calculatorApp2/calculatorApp2/CalculatorApp.aspx.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Eventing.Reader;
+using System.Drawing;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace calculatorApp2
+{
+    public partial class CalculatorApp : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            
+
+        }
+
+        protected void Button_Click(object sender, EventArgs e)
+        {
+            string firstnumber = firstNum.Text;
+            string secondnumber = secondNum.Text;
+            
+
+            Button ButtonClicked = sender as Button;
+
+            if (ButtonClicked != null && int.TryParse(firstnumber, out int number1) && int.TryParse(secondnumber, out int number2))
+            {
+                Output.ForeColor = default;
+                int output;
+                switch (ButtonClicked.ID)
+                {
+                    case "additionBtn":
+                        output  = number1 + number2;
+                        Output.Text = output.ToString();
+                        break;
+
+                    case "divisionBtn":
+                        output = number1 / number2;
+                        Output.Text = output.ToString();
+                        break;
+
+                    case "subtractionBtn":
+                        output = number1 - number2;
+                        Output.Text = output.ToString();
+                        break;
+
+                    case "multiplicationBtn":
+                        output = number1 * number2;
+                        Output.Text = output.ToString();
+                        break;
+                }
+
+            }
+            else
+            {
+                firstNum.Text = null;
+                secondNum.Text = null;
+                Output.Text = "Please enter Numbers";
+                Output.ForeColor = Color.Red;
+            }
+        }
+    }
+}
